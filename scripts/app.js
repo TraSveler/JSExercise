522c8c7361d4c6f76814f9621bf997a1ea7a3f3e
makas <maksgordiichuk97@gmail.com>
2015-12-22 17:28:21 +0200

    Final release with removing photos

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,28 @@
-/**
- * Created by makaS on 12/5/2015.
- */
+document.getElementById( 'allPic' ).addEventListener( 'click', function (ev) {
+
+    if (ev.target.tagName === 'IMG') {
+        var howMany = this.querySelectorAll( 'li' ).length;
+
+        if ( howMany > 1 ) {
+            var parentElement = ev.target.parentNode;
+            this.removeChild(parentElement);
+        } else {
+            document.querySelector('p').innerHTML = "<p>Вы выбрали " + ev.toElement.alt + "</p>"
+        }
+    }
+
+});
+
+
+
+
+
+
+
+/*document.getElementById( 'allDiv' ).addEventListener( 'click', function (event) {
+	var howMany = this.querySelectorAll('img').lenght;
+
+	var parentElement = event.target.parentNode;
+	this.removeChild(parentElement);
+});
+*/
